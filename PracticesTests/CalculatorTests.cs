f97e572afe3a78eccd7c5e14698fcec2da96f1e2
olgasenina <o.v.senina2020@gmail.com>
2022-03-31 20:01:03 +0300

    Добавьте файлы проекта.

---
+++ PracticesTests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Practices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticesTests
+{
+    /// <summary>
+    /// Тестируем функции калькулятора
+    /// </summary>
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(3, 5) == 8);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(9, 8) == 1);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(3, 4) == 12);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(88, 22) == 4);
+        }
+    }
+}
